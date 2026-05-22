4b99b484ca0f89048aab4cb8e1fccb2a9b8baa7a
Hliceath <github@hliceath.com>
2026-05-22 16:57:53 +0200

    chore: refactor file

--- source/main.c
+++ source/main.c
@@ -3,45 +3,9 @@
 #include <string.h>
 #include <sys/stat.h>
 
-#define CHMOD (0755)
+#include "utils.h"
 
-void create_file(char* path) {
-    FILE* file = fopen(path, "w");
-
-    if (!file) {
-        printf("error: failed to open file '%s' for writing!", path);
-        exit(-1);
-    }
-
-    fclose(file);
-}
-
-void create_subdirs(char* root_dir) {
-    const char* subdirs[4] = {"/bin", "/build", "/include", "/source"};
-    const char main_file[8] = "/main.c";
-    const char* root_files[2] = {"/Makefile", "/.clang-format"};
-
-    for (int i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); i++) {
-        char path_subdir[280] = "";
-        strcpy(path_subdir, root_dir);
-        strcat(path_subdir, subdirs[i]);
-
-        if (mkdir(path_subdir, CHMOD) == -1) {
-            printf("error: folder already exists.\n");
-            exit(-1);
-        }
-
-        if (strcmp(subdirs[i], "/source") == 0) {
-            create_file(strcat(path_subdir, main_file));
-        }
-    }
-
-    for (int i = 0; i < (sizeof(root_files) / sizeof(root_files[0])); i++) {
-        char path_file[267] = "";
-        strcpy(path_file, root_dir);
-        create_file(strcat(path_file, root_files[i]));
-    }
-}
+void create_template(char* root_dir);
 
 int main(int argc, const char* argv[]) {
     char root_dir[253] = "./";
@@ -52,7 +16,7 @@ int main(int argc, const char* argv[]) {
     }
 
     if (strcmp(argv[1], "new") == 0) {
-        printf("creating a new C/C++ project under the name: %s\n", argv[2]);
+        printf("creating a new C/C++ template \"%s\"\n", argv[2]);
     } else {
         printf("invalid command: use the keyword 'new' to create a new "
                "project: 'nenecchi new <project_name>.'\n");
@@ -64,13 +28,35 @@ int main(int argc, const char* argv[]) {
         exit(-1);
     }
 
-    if (mkdir(argv[2], CHMOD) == -1) {
-        printf("error: folder already exists.\n");
-        exit(-1);
-    }
+    create_directory(argv[2]);
     strcat(root_dir, argv[2]);
-
-    create_subdirs(root_dir);
+    create_template(root_dir);
+    printf("note: for a c++ project make sure to rename name.c -> name.cpp\n");
 
     return 0;
+}
+
+void create_template(char* root_dir) {
+    const char* subdirs[4] = {"/bin", "/build", "/include", "/source"};
+    const char main_file[8] = "/main.c";
+    const char* root_files[2] = {"/Makefile", "/.clang-format"};
+
+    for (int i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); i++) {
+        char path_subdir[280] = "";
+
+        strcpy(path_subdir, root_dir);
+        strcat(path_subdir, subdirs[i]);
+        create_directory(path_subdir);
+
+        if (strcmp(subdirs[i], "/source") == 0) {
+            create_file(strcat(path_subdir, main_file));
+        }
+    }
+
+    for (int i = 0; i < (sizeof(root_files) / sizeof(root_files[0])); i++) {
+        char path_file[267] = "";
+
+        strcpy(path_file, root_dir);
+        create_file(strcat(path_file, root_files[i]));
+    }
 }
