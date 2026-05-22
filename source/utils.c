ad99bbbfcd5e18ebe73cfa5c8a6cb797b09168c5
Hliceath <github@hliceath.com>
2026-05-22 23:40:50 +0200

    feat: write content when creating files

--- source/utils.c
+++ source/utils.c
@@ -4,20 +4,23 @@
 
 #include "utils.h"
 
-void create_file(char* path) {
+void create_file(char* path, const char* content) {
     FILE* file = fopen(path, "w");
 
     if (!file) {
-        printf("error: failed to open file '%s' for writing!", path);
+        printf(RED "error" RESET ": failed to open file '%s' for writing!",
+               path);
         exit(-1);
     }
+
+    fputs(content, file);
 
     fclose(file);
 }
 
 void create_directory(const char* path) {
     if (mkdir(path, CHMOD) == -1) {
-        printf("error: directory already exists.\n");
+        printf(RED "error" RESET ": failed creating '%s' directory.\n", path);
         exit(-1);
     }
 }
