8b41071c2ed5b0ba1cc50473852784fd9eb97697
Hliceath <github@hliceath.com>
2026-05-22 23:41:27 +0200

    feat: add content to write in files

--- source/main.c
+++ source/main.c
@@ -4,41 +4,50 @@
 
 #include "utils.h"
 
-void create_template(char* root_dir);
+void create_template_subdirs(char* root_dir);
 
 int main(int argc, const char* argv[]) {
     char root_dir[253] = "./";
 
     if (argc != 3) {
-        printf("error: number of arguments must be 2.\n");
+        printf(RED "error" RESET ": number of arguments must be 2.\n");
         exit(-1);
     }
 
     if (strcmp(argv[1], "new") == 0) {
-        printf("creating a new C/C++ template \"%s\"\n", argv[2]);
+        printf(GREEN "creating" RESET " a new C/C++ template \"%s\"\n",
+               argv[2]);
     } else {
-        printf("invalid command: use the keyword 'new' to create a new "
-               "project: 'nenecchi new <project_name>.'\n");
+        printf(RED "invalid command" RESET
+                   ": use the keyword 'new' to create a new "
+                   "project: 'nenecchi new <project_name>.'\n");
         exit(-1);
     }
 
     if (sizeof(argv[2]) > 250) {
-        printf("error: name of folder too long (limit 250 chars)\n");
+        printf(RED "error" RESET
+                   ": name of folder too long (limit 250 chars)\n");
         exit(-1);
     }
 
     create_directory(argv[2]);
     strcat(root_dir, argv[2]);
-    create_template(root_dir);
-    printf("note: for a c++ project make sure to rename name.c to name.cpp\n");
+    create_template_subdirs(root_dir);
+    printf(BLUE "note" RESET
+                ": for a c++ project make sure to rename main.c to main.cpp\n");
 
     return 0;
 }
 
-void create_template(char* root_dir) {
+void create_template_subdirs(char* root_dir) {
     const char* subdirs[4] = {"/bin", "/build", "/include", "/source"};
     const char main_file[8] = "/main.c";
     const char* root_files[2] = {"/Makefile", "/.clang-format"};
+    const char main_content[105] = "#include <stdio.h>\n\n"
+                                   "int main(int argc, const char* argv[]) {"
+                                   "\n    printf(\"new game!\");\n"
+                                   "    return 0;\n"
+                                   "}";
 
     for (int i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); i++) {
         char path_subdir[280] = "";
@@ -48,7 +57,7 @@ void create_template(char* root_dir) {
         create_directory(path_subdir);
 
         if (strcmp(subdirs[i], "/source") == 0) {
-            create_file(strcat(path_subdir, main_file));
+            create_file(strcat(path_subdir, main_file), main_content);
         }
     }
 
@@ -56,6 +65,6 @@ void create_template(char* root_dir) {
         char path_file[267] = "";
 
         strcpy(path_file, root_dir);
-        create_file(strcat(path_file, root_files[i]));
+        create_file(strcat(path_file, root_files[i]), "# your config here");
     }
 }
