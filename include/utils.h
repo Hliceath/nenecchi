8bf4249afc2b596668a6a97eb0c6aa38abcffc91
Hliceath <github@hliceath.com>
2026-05-24 23:10:55 +0200

    feat: add version and help commands

--- include/utils.h
+++ include/utils.h
@@ -7,5 +7,9 @@
 #define BLUE "\x1b[36m"
 #define RESET "\x1b[0m"
 
+#define VERSION "v1.1.0"
+
 void create_file(char* path, const char* content);
 void create_directory(const char* path);
+void show_version();
+void show_help();
